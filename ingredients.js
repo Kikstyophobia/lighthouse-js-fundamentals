0dbedf313c4ae1b266a41602f0dc525079c42117
Calvin Chung <calvin-chung@live.com>
2022-02-04 16:12:14 -0800

    ingredients

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+let i = 0
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+let i = 0
+while (i < ingredients.length) {
+  i++;
+}
+console.log(ingredients.reverse(i));
